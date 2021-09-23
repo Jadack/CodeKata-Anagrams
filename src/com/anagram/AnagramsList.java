cc72f3d734521bd466c6146b470019cf1272c103
Jadack <jackdamiolixp@gmail.com>
2021-09-22 20:32:23 -0400

    Unit test imports and implementation repairde with gitignore.

--- src/com/anagram/AnagramsList.java
+++ src/com/anagram/AnagramsList.java
@@ -1,5 +1,5 @@
 package com.anagram;
 
-public class AnagramList {
+public class AnagramsList {
   
 }
--- test/AnagramsListTest.java
+++ test/AnagramsListTest.java
@@ -1,35 +1,46 @@
-import com.anagram.AnagramList;
+import com.anagram.AnagramsList;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
 
 public class AnagramListTest {
 
   @Test
   public void testIfOneEntryReturnEmptyCollections() {
     final String[] wordList = {"testWord"};
-    final Stream<String> wordStream = Arrays.Stream(wordList);
-    assertEquals(0, AnagramList.anagramsListInsideFile(wordStream).size());
+    final Stream<String> wordStream = Arrays.stream(wordList);
+    assertEquals(0, AnagramsList.anagramsListInsideFile(wordStream).size());
   }
 
   @Test
   public void testIfTwoNotAnagramsStringsReturnEmptyCollection() {
     final String[] wordList = {"sinks", "enlist"};
-    final Stream<String> wordStream = Arrays.Stream(wordList);
-    assertEquals(0, AnagramList.anagramsListInsideFile(wordStream).size());
+    final Stream<String> wordStream = Arrays.stream(wordList);
+    assertEquals(0, AnagramsList.anagramsListInsideFile(wordStream).size());
   }
 
   @Test
   public void testIfTwoAnagramsStringsReturnOneCollection() {
     final String[] wordList = {"fresher", "refresh"};
-    final Stream<String> wordStream = Arrays.Stream(wordList);
-    assertEquals(1, AnagramList.anagramsListInsideFile(wordStream).size());
+    final Stream<String> wordStream = Arrays.stream(wordList);
+    assertEquals(1, AnagramsList.anagramsListInsideFile(wordStream).size());
   }
 
   @Test
-  public void testIfReturnAnagramsOfInputFile() {
+  public void testIfReturnAnagramsOfInputFile() throws FileNotFoundException {
     final File fileName = new File("resources/wordList.txt");
     final FileReader inputFile = new FileReader(fileName);
-    final BufferReader reader = new BufferReader(inputFile);
-    final Collection<Array<String>> anagramsInsideFile = AnagramList.anagramsInsideFile(reader.lines());
+    final BufferedReader reader = new BufferedReader(inputFile);
+    final Collection<ArrayList<String>> anagramsInsideFile = AnagramsList.anagramsInsideFile(reader.lines());
     assertEquals(20683, anagramsInsideFile.size());
   }
 }
